ae1d2476a031fe7b610fcbd04bf269d65e9e26f6
tamphan1506 <tamptm19411@st.uel.edu.vn>
2021-12-11 14:36:30 +0700

    adapter

--- app/src/main/java/com/example/model/Product.java
+++ app/src/main/java/com/example/model/Product.java
@@ -5,4 +5,43 @@ public class Product {
     private String productName;
     private String productDes;
     private int productImage;
+
+    public Product(int productId, String productName, String productDes, int productImage) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDes = productDes;
+        this.productImage = productImage;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDes() {
+        return productDes;
+    }
+
+    public void setProductDes(String productDes) {
+        this.productDes = productDes;
+    }
+
+    public int getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(int productImage) {
+        this.productImage = productImage;
+    }
 }
