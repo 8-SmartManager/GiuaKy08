7eb7d3624b0d6c635845b8b4f14d43b8f4e36ae5
tamphan1506 <tamptm19411@st.uel.edu.vn>
2021-12-11 14:44:36 +0700

    adapter updata

--- app/src/main/java/com/example/giuaky08/MainActivity.java
+++ app/src/main/java/com/example/giuaky08/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.giuaky08;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.model.Product;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvProduct;
+    Product adapter;
+    ArrayList<Product> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinkViews();
+    }
+
+    private void LinkViews() {
+        lvProduct=findViewById(R.id.lvProduct);
     }
 }
